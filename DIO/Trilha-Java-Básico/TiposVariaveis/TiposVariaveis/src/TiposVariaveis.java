8615e194793c97ad7799323889f106e63d931bbf
Gust4av0 <gustavomarcolin2005@gmail.com>
2024-08-01 22:22:45 -0300

    Curso dio

---
+++ DIO/Trilha-Java-Básico/TiposVariaveis/TiposVariaveis/src/TiposVariaveis.java
@@ -0,0 +1,12 @@
+public class TiposVariaveis {
+    public static void main(String[] args){
+        int numero = 10;
+        numero = 50; 
+
+        System.out.println(numero);
+
+        final double VALOR_PI = 3.14;
+
+        System.out.println(VALOR_PI);
+    }
+}
