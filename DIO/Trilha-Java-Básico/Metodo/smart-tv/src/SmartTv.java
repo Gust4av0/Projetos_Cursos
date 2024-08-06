80ce21bb2dd27186176e5892b7aa33d86049427c
Gust4av0 <gustavomarcolin2005@gmail.com>
2024-08-06 17:44:09 -0300

    Exercicio de metodos

---
+++ DIO/Trilha-Java-Básico/Metodo/smart-tv/src/SmartTv.java
@@ -0,0 +1,12 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class SmartTv {
+    static boolean ligada =false;
+    int canal  = 1;
+    int volume = 15;
+
+    public void ligar(){
+        ligada = true;
+    }
+
+}
