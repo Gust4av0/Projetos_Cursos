5b1f3a4c64c4c07bfffa960768276f16009a64ac
Gust4av0 <gustavomarcolin2005@gmail.com>
2024-08-08 10:00:24 -0300

    finalizando desafio

--- DIO/Trilha-Java-Básico/DesafioProjeto/ContaBancaria/conta-banco/src/ContaTerminal.java
+++ DIO/Trilha-Java-Básico/DesafioProjeto/ContaBancaria/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int conta;
+        String agencia;
+        String nome;
+        double saldo = 1000.50; 
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        conta = entrada.nextInt();
+        System.out.println("Por favor, digite o número da Agência: ");
+        agencia = entrada.next();
+        System.out.println("Por favor, digite seu Nome: ");
+        nome = entrada.next();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + conta + ", e seu saldo " + saldo + ", já está disponível para saque.");
     }
 }
